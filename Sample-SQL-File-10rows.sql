9b9054acb8e78c1c243069451777fd46a6434764
nakularsenal <134908888+nakularsenal@users.noreply.github.com>
2023-06-05 12:25:11 +0530

    Update Sample-SQL-File-10rows.sql
    script changes

--- Sample-SQL-File-10rows.sql
+++ Sample-SQL-File-10rows.sql
@@ -1,12 +1,4 @@
---
--- Database: `samplevideo_db`
---
 
--- --------------------------------------------------------
-
---
--- Table structure for table `user_details`
---
 
 CREATE TABLE IF NOT EXISTS `user_details` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -19,9 +11,6 @@ CREATE TABLE IF NOT EXISTS `user_details` (
   PRIMARY KEY (`user_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10001 ;
 
---
--- Dumping data for table `user_details`
---
 
 INSERT INTO `user_details` (`user_id`, `username`, `first_name`, `last_name`, `gender`, `password`, `status`) VALUES
 (1, 'rogers63', 'david', 'john', 'Female', 'e6a33eee180b07e563d74fee8c2c66b8', 1),
