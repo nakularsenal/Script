14fcbe60e2dc17e2e66557c728fb62f771e6b926
nakularsenal <134908888+nakularsenal@users.noreply.github.com>
2023-06-06 11:24:07 +0530

    Update Sample-SQL-File-10rows.sql
    scriptadded

--- Sample-SQL-File-10rows.sql
+++ Sample-SQL-File-10rows.sql
@@ -1,1 +1,2 @@
-CREATE TABLE Persons2(PersonID int,LastName varchar(255),FirstName varchar(255),Address varchar(255),City varchar(255));
+CREATE TABLE Persons3(PersonID int,LastName varchar(255),FirstName varchar(255),Address varchar(255),City varchar(255));
+INSERT INTO Persons3(PersonID,LastName,FirstName,Address,City)VALUES (1, 'Tom B. Erichsen', 'Skagen', 'Street no10','Norway'),
